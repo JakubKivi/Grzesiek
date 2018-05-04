e32ffea6f10528794494b399439943e79adaad2a
JakubKivi <panmcleon@gmail.com>
2018-05-05 00:19:14 +0200

    passed bluetooth test and created a phone UI

--- Tests/bluetoothTest/bluetoothTest.ino
+++ Tests/bluetoothTest/bluetoothTest.ino
@@ -1,9 +1,35 @@
-void setup(){
+#include <Servo.h>
+
+char data[100];
+byte dataIndex;
+char sign;
+
+Servo servo;
+
+void setup() {
   Serial1.begin(9600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-}
-void loop(){
-  if(Serial1.read()==1) digitalWrite(13, HIGH);
+  servo.attach(11);
 }
 
+void exeData(){
+  if(data[0]== 's'){
+    unsigned int val = atof(data+2);
+    servo.write(val);
+  }
+}
+
+void loop() {
+  if(Serial1.available() > 0){
+    sign = Serial1.read();
+    if (sign == '\n'){
+      data[dataIndex] = 0;
+      exeData();
+      dataIndex = 0;
+    }else{
+      data[dataIndex] = sign;
+      if(dataIndex < 99)dataIndex++;
+    }
+  }
+  
+
+}
